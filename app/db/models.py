a9fd8d7a4614b401e36060e62f87c4ee5a2f52b7
mattc101 <mcyc926@gmail.com>
2024-11-06 17:48:18 -0500

    added Events table to models.py

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,6 @@
 from app.db.db import engine
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 
 """
 <app/db/models.py>
@@ -17,5 +17,18 @@ class Todo(Base):
     id = Column(Integer, primary_key=True)
     text = Column(String)
     is_done = Column(Boolean, default=False)
+
+class Events(Base):
+    __tablename__ = "Events"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    photo = Column(String)
+    location = Column(String)
+    description = Column(String)
+    type = Column(String)
+    start_at = Column(DateTime)
+    end_at = Column(DateTime)
+    group_name = Column(String)
 
 Base.metadata.create_all(engine)
