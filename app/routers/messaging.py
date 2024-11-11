518e7bf18e49bb64cfc89ff0ae1f17a265a60588
aiapo <agitschl@umflint.edu>
2024-11-11 16:07:19 -0500

    Comment and describe what's happening

--- app/routers/messaging.py
+++ app/routers/messaging.py
@@ -14,19 +14,23 @@ router = APIRouter()
 
 @router.get("/announcements/get/{items}")
 def getMessages(items):
-    index = 0
-
+    # create a list of all announcements
     allAnnouncements=[]
+
+    # get from the umflint api, iterate for each
     for announcement in get_announcement_items()['data']:
+        # some clean up for getting the start date (some only have publish at)
         if announcement['display_start']!="":
             dateStart = announcement['display_start']
         else:
             dateStart = announcement['published_at']
 
+        # get all the roles
         roles = []
         for role in announcement['affiliations']:
             roles.append({'role': getRoleId(role['name'])})
 
+        # create a dictionary of an individual announcement
         announcementJson = {
                     'id': announcement['id'],
                     'title': announcement['title'],
@@ -36,14 +40,17 @@ def getMessages(items):
                     'roles': roles
                 }
 
+        # add the dictionary to the list
         allAnnouncements.append(announcementJson)
-        index = index+1
 
+    # get from the database, iterate for each
     for announcement in session.query(Announcements).all():
+        # get all the roles (and since it's comma seperated, iterate)
         roles = []
         for role in announcement.role.split(","):
             roles.append({'role': role})
 
+        # create a dictionary of an individual announcement
         announcementJson = {
                     'id': announcement.id,
                     'title': announcement.title,
@@ -53,16 +60,19 @@ def getMessages(items):
                     'roles': roles
                 }
 
+        # add the dictionary to the list
         allAnnouncements.append(announcementJson)
 
+    # sort by newest start date
     allAnnouncements.sort(key=lambda d: datetime.strptime(d['dateStart'], "%Y-%m-%d %H:%M:%S"), reverse=True)
 
+    # if the query is a number, return by a max number of items, otherwise all
     if(items.isnumeric()):
         return allAnnouncements[:int(items)]
     else:
         return allAnnouncements
 
-
+# based on the name, give a role id to match database
 def getRoleId(name):
     if name=="student":
         return 1
