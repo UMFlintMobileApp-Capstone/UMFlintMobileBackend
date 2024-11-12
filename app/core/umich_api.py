d2e878d288c7f86d22ba68d4f6fc6b8560ea957b
mattc101 <mcyc926@gmail.com>
2024-11-11 19:45:43 -0500

    add .json to api because I forgot

--- app/core/umich_api.py
+++ app/core/umich_api.py
@@ -7,7 +7,7 @@ def get_announcement_items():
     return r.get('https://www.umflint.edu/wp-json/wp-content-types/announcements').json()
 
 def get_events_items(slug):
-    return r.get("https://events.umflint.edu/api/wp-block/groups/" + slug + "/events")
+    return r.get("https://events.umflint.edu/api/wp-block/groups/" + slug + "/events").json()
 
 def get_events_groups():
-    return r.get("https://events.umflint.edu/api/wp-block/groups/all")
+    return r.get("https://events.umflint.edu/api/wp-block/groups/all").json()
