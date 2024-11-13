49956c4250d2283f1c660a9b71bf229de6d4e20d
aiapo <16511183+aiapo@users.noreply.github.com>
2024-11-12 21:23:09 -0500

    Add a get user by email route

--- app/routers/user.py
+++ app/routers/user.py
@@ -12,8 +12,14 @@ from fastapi import APIRouter
 # create the router
 router = APIRouter()
 
-@router.get("/user/{id}")
+@router.get("/user/byId/{id}")
 def getUser(id: int, token: str):
     getUserDetails(token)
     
     return session.query(User).filter(User.id==id).all()
+
+@router.get("/user/byEmail/{email}")
+def getUser(email: int, token: str):
+    getUserDetails(token)
+    
+    return session.query(User).filter(User.email==email).all()
