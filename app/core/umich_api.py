f1ed47e86a4773438304a23ce556ad3e85064da6
mknox13 <knoxmichael545@gmail.com>
2024-11-21 21:26:38 -0500

    added academic events route

--- app/core/umich_api.py
+++ app/core/umich_api.py
@@ -6,8 +6,11 @@ def get_news_items():
 def get_announcement_items():
     return r.get('https://www.umflint.edu/wp-json/wp-content-types/announcements').json()
 
-def get_events_items(slug):
+def get_events_items():
     return r.get("https://events.umflint.edu/api/events/promoted-feed").json()
 
 def get_events_groups():
     return r.get("https://events.umflint.edu/api/wp-block/groups/all").json()
+
+def get_academic_events():
+    return r.get("https://www.umflint.edu/registrar/wp-json/wp-content-types/academic-events/current").json()
